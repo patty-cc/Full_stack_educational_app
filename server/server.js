09e001296c8698723b606bf50ac2d118935aec37
Simon Anger <simonanger89@gmail.com>
2017-10-27 15:33:25 +0100

    Finished api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-var getAllAnimals = require("./db/db.js");
+var getAllAnimals = require("./db/db_facts.js");
+var getAllQuiz = require("./db/db_quiz.js");
 
 app.get("/api/animals", function(req, res) {
   getAllAnimals(function(data) {
@@ -8,6 +9,15 @@ app.get("/api/animals", function(req, res) {
     res.json(data);
   });
 })
+
+app.get("/api/quiz", function(req, res){
+  getAllQuiz(function(data){
+    res.json(data);
+  })
+})
+
+
+
 
 app.listen(3000, function() {
   console.log("it worked");
