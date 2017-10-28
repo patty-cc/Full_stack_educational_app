5beea06f54ae56e101e76a8f8d41b5a66c01e08c
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-28 11:33:43 +0100

    Can display index.html and styles.css

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
 var express = require("express");
 var app = express();
+var path = require('path');
 var getAllAnimals = require("./db/db_facts.js");
 var getAllQuiz = require("./db/db_quiz.js");
+
+app.use(express.static(__dirname + '/../client/build'));
 
 app.get("/api/animals", function(req, res) {
   getAllAnimals(function(data) {
 
     res.json(data);
   });
-})
+});
 
 app.get("/api/quiz", function(req, res){
   getAllQuiz(function(data){
     res.json(data);
   })
-})
+});
 
-
-
+app.get("/quiz/jungle", function(req, res) {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
 
 
 app.listen(3000, function() {
   console.log("it worked");
-})
+});
