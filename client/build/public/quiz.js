5d14693b7ff73679bf5aa4a28ca67b36e8bf2f50
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-29 19:24:26 +0000

    Answers displayed in random order without repeats

--- client/build/public/quiz.js
+++ client/build/public/quiz.js
@@ -1,3 +1,6 @@
+var jungleSets;
+var indexes = [];
+
 var displayWelcomeText = function() {
   var textBox = document.getElementById("text-box");
   var pTag = document.createElement("p");
@@ -15,7 +18,7 @@ var createNextButton = function() {
 }
 
 var incorrectAnswerClicked = function(){
-  this.style.visibility = "hidden"
+  this.style.visibility = "hidden";
   var textBoxText = document.getElementById("text-box-text");
   textBoxText.innerText = "Wrong! Try again!";
 }
@@ -26,32 +29,52 @@ var correctAnswerClicked = function(){
   createNextButton();
 }
 
-var initializeQuiz = function(data) {
+var initializeQuiz = function(indexesArray) {
+  // get random index to select a random set from the question-answers array
+  var index = getRandomArrayIndex(indexesArray);
+
+// create h1 element displaying the question from the selected set
   var journalDiv = document.getElementById("quiz-journal-box");
   var questionText = document.createElement("h1");
-  questionText.innerText = data[0].question;
+  questionText.innerText = jungleSets[index].question;
   journalDiv.appendChild(questionText);
-  console.log("question: ", data[0].question);
+  console.log("question: ", jungleSets[index].question);
 
-  var jaguar = document.createElement("img");
-  jaguar.setAttribute("id", "quiz-img");
-  jaguar.src = data[0].img;
-  journalDiv.appendChild(jaguar);
+// display image from the selected set
+  var pic = document.createElement("img");
+  pic.setAttribute("id", "quiz-img");
+  pic.src = jungleSets[index].img;
+  journalDiv.appendChild(pic);
 
+// create a div for the three answers to be displayed
   var answersWrapper = document.createElement("div");
   answersWrapper.setAttribute("id", "quiz-answers-wrapper");
   journalDiv.appendChild(answersWrapper);
 
+// since there are always be three possible answers to a question I create an array of indexes for them
+  var answersIndexes = [0, 1, 2];
+// I choose random indexes of the questions to be displayed so that the order
+// of answers will be different every time you view the question
+  var firstIndex = getRandomArrayIndex(answersIndexes);
+  var answer1Index = answersIndexes[firstIndex];
+  answersIndexes.splice(firstIndex, 1);
+  var secondIndex = getRandomArrayIndex(answersIndexes);
+  var answer2Index = answersIndexes[secondIndex];
+  answersIndexes.splice(secondIndex, 1);
+  var answer3Index = answersIndexes[0];
+
+
+// create buttons with 3 questions using the indexes that have just been generated
   var answer1 = document.createElement("button");
-  answer1.innerText = data[0].correctAnswer[0];
+  answer1.innerText = jungleSets[index].answers[answer1Index].text;
   answersWrapper.appendChild(answer1);
 
   var answer2 = document.createElement("button");
-  answer2.innerText = data[0].incorrectAnswer[0];
+  answer2.innerText = jungleSets[index].answers[answer2Index].text;
   answersWrapper.appendChild(answer2);
 
   var answer3 = document.createElement("button");
-  answer3.innerText = data[0].incorrectAnswer[1];
+  answer3.innerText = jungleSets[index].answers[answer3Index].text;
   answersWrapper.appendChild(answer3);
 
   answer1.addEventListener("click", correctAnswerClicked);
@@ -78,6 +101,27 @@ var displayJournal = function() {
   displayHomeButton();
 }
 
+var getRandomArrayIndex = function(array){
+    return Math.floor(Math.random()*(array.length));
+}
+
+
+var getIndexes = function(){
+  for (var i = 0; i < jungleSets.length; i++){
+    indexes.push(i);
+  }
+  console.log("indexes: ", indexes);
+  initializeQuiz(indexes);
+}
+
+var getJungleSets = function(data){
+  jungleSets = data.filter(function(set){
+    return set.habitat === "Jungle";
+  });
+  console.log("jungleSets: ", jungleSets);
+  getIndexes();
+}
+
 var makeRequest = function(url, callback) {
   var request = new XMLHttpRequest();
     request.open( "GET", url);
@@ -93,7 +137,7 @@ var makeRequest = function(url, callback) {
 var initialize = function() {
   displayWelcomeText();
   displayJournal();
-  makeRequest("http://localhost:3000/api/quiz", initializeQuiz);
+  makeRequest("http://localhost:3000/api/quiz", getJungleSets);
 }
 
 window.addEventListener("load", initialize);
