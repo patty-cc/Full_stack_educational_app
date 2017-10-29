665bb27c2bc8aa006cae8ea5739ac7434e4b2b55
davebur26 <davebur26@gmail.com>
2017-10-29 12:19:53 +0000

    add book which shows info for each animal

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -68,35 +68,58 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 var requestHelper = __webpack_require__(1);
+
+var renderAnimalsInfo = function(animals, counter) {
+  var animalPic = document.getElementById('animalImg')
+
+  animalPic.src = animals[counter].img
+  animalPic.height = '200'
+  animalPic.width = '200'
+
+  var animalType = document.getElementById('animalType');
+  var animalTypeText = document.getElementById('animal-type-text')
+  if (!animalTypeText) {
+    animalTypeText = document.createElement('p')
+    animalTypeText.id = 'animal-type-text'
+  }
+
+  var animalGroup = document.getElementById('animalGroup');
+  var animalGroupText = document.getElementById('animal-group-text')
+  if (!animalGroupText) {
+    animalGroupText = document.createElement('p')
+    animalGroupText.id = 'animal-group-text'
+  }
+
+  var animalFunfact = document.getElementById('animalFunfact');
+  var animalFunfactText = document.getElementById('animal-funfact-text')
+  if (!animalFunfactText) {
+    animalFunfactText = document.createElement('p')
+    animalFunfactText.id = 'animal-funfact-text'
+  }
+
+
+
+  animalTypeText.innerText = animals[counter].type;
+  animalGroupText.innerText = animals[counter].group;
+  animalFunfactText.innerText = animals[counter].funFact;
+
+  animalType.appendChild(animalTypeText)
+  animalGroup.appendChild(animalGroupText)
+  animalFunfact.appendChild(animalFunfactText)
+}
+
 window.addEventListener('DOMContentLoaded', function() {
-
   requestHelper.getRequest('http://localhost:3000/api/animals', function(animals) {
-    // console.log(animals);
 
-  var animalPic = document.getElementById('animal-picture-wrapper')
-  console.log(animals[0].img);
+    counter = 0
+    renderAnimalsInfo(animals, counter)
+    counter ++
 
-  var animalImage = document.createElement('img')
-  animalImage.src = animals[0].img
-  animalImage.height = '200'
-  animalImage.width = '200'
-  animalPic.appendChild(animalImage)
-
-  var animalInfo = document.getElementById('animal-info')
-
-  var animalType = document.createElement('p');
-  var animalGroup = document.createElement('p');
-  var animalFunfact = document.createElement('p');
-
-  animalType.innerText = 'Type: ' + animals[0].type;
-  animalGroup.innerText = 'Group Name: ' + animals[0].group;
-  animalFunfact.innerText = 'Fun Fact: ' + animals[0].funFact;
-
-  animalInfo.appendChild(animalType)
-  animalInfo.appendChild(animalGroup)
-  animalInfo.appendChild(animalFunfact)
-
-
+    var nextButton = document.getElementById('button')
+    nextButton.addEventListener('click', function(){
+      renderAnimalsInfo(animals, counter)
+      counter ++
+    })
   })
 })
 
