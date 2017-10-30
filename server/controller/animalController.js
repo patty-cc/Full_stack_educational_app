51bbae76b0f0c7dd33d292731d1b2748cb91cc6d
davebur26 <davebur26@gmail.com>
2017-10-30 10:17:59 +0000

    Rename jungle API route

--- server/controller/animalController.js
+++ server/controller/animalController.js
@@ -2,9 +2,8 @@ var getAllAnimals = require('../db/db_facts.js');
 var express = require("express");
 var animalRouter = express.Router()
 
-animalRouter.get('/', function (req,res) {
+animalRouter.get('/jungle', function (req,res) {
   getAllAnimals(function(data) {
-
     res.json(data)
   })
 })
