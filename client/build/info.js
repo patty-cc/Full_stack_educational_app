5ba733065d1b11e6f35482607e4ee59da723ea1c
davebur26 <davebur26@gmail.com>
2017-10-31 14:07:42 +0000

    add book to info page

--- client/build/info.js
+++ client/build/info.js
@@ -21,20 +21,143 @@ var renderJungleInfo = function(data){
 
 
 
-  createHtml()
+  //createHtml()
 
-  counter = 0
-  renderAnimalsInfo(data, counter)
-  counter ++
+//  counter = 0
+//  renderAnimalsInfo(data, counter)
+  //counter ++
 
 //add if statement here
-  var nextButton = document.getElementById('button')
-  nextButton.addEventListener('click', function(){
-    renderAnimalsInfo(data, counter)
-    counter ++
-  })
+
+htmlSetup(data)
+var page = 0;
+var forwards = true;
+var updatePages = function() {
+  var pages = document.querySelectorAll(".page");
+  pages.forEach(function(element, index) {
+    if (page === index) {
+      element.className = "page active";
+    } else if (index < page) {
+      element.className = "page flipped";
+    } else {
+      element.className = "page";
+    }
+  });
+};
+
+document.querySelector(".book").addEventListener("click", function(event) {
+  if (event.target.className.includes("front")) {
+    page++;
+    updatePages();
+  } else {
+    page--;
+    updatePages();
+  }
+});
+
+
+
+
+  // var nextButton = document.getElementById('button')
+  // nextButton.addEventListener('click', function(){
+  //   renderAnimalsInfo(data, counter)
+  //   counter ++
+  // })
 }
 
+// new code
+var htmlSetup = function (animalData) {
+  var backgroundImageWrapper = document.getElementById('background-image-wrapper')
+
+  var sceneDiv = document.createElement('div')
+  sceneDiv.className = "scene"
+  backgroundImageWrapper.appendChild(sceneDiv)
+  var bookArticle = document.createElement('article')
+  bookArticle.className = "book"
+  sceneDiv.appendChild(bookArticle)
+  var pageSection = document.createElement('section')
+  pageSection.className = "page active"
+  bookArticle.appendChild(pageSection)
+    var frontDiv = document.createElement('div')
+    frontDiv.className = "front"
+    pageSection.appendChild(frontDiv)
+      var h1 = document.createElement('h1')
+      frontDiv.appendChild(h1)
+      h1.innerText = "Hello Front"
+      var p = document.createElement('p')
+      frontDiv.appendChild(p)
+
+// var animals = ["Animal 1", "Animal 2", "Animal 3"]
+
+for (animal of animalData) {
+    var backDiv = document.createElement('div')
+    backDiv.className = "back"
+    pageSection.appendChild(backDiv)
+      var animalImg = document.createElement('img')
+      animalImg.id = "animal-img"
+      // animalImg.src = animal.img
+      backDiv.appendChild(animalImg)
+      var animalImgText = document.createElement('p')
+      animalImgText.id = "animal-name"
+      animalImgText.innerText = animal.name
+      backDiv.appendChild(animalImgText)
+  var pageSection = document.createElement('section')
+  pageSection.className = "page"
+  bookArticle.appendChild(pageSection)
+    var frontDiv = document.createElement('div')
+    frontDiv.className = "front"
+    pageSection.appendChild(frontDiv)
+
+      var animalType = document.createElement('div')
+      animalType.id = "animal-type"
+      frontDiv.appendChild(animalType)
+        var infoHeading = document.createElement('p')
+        infoHeading.id = 'infoHeading'
+        infoHeading.innerText = "Type of Animal"
+        animalType.appendChild(infoHeading)
+        var animalTypeText = document.createElement('p')
+        animalTypeText.id = "animal-type-text"
+        animalTypeText.innerText = animal.type
+        animalType.appendChild(animalTypeText)
+
+      var animalGroup = document.createElement('div')
+      animalGroup.id = "animal-group"
+      frontDiv.appendChild(animalGroup)
+        var infoHeading = document.createElement('p')
+        infoHeading.id = 'infoHeading'
+        infoHeading.innerText = "Group Name"
+        animalGroup.appendChild(infoHeading)
+        var animalGroupText = document.createElement('p')
+        animalGroupText.id = "animal-group-text"
+        animalGroupText.innerText = animal.group
+        animalGroup.appendChild(animalGroupText)
+
+      var animalFunFact = document.createElement('div')
+      animalFunFact.id = "animal-fun-fact"
+      frontDiv.appendChild(animalFunFact)
+        var infoHeading = document.createElement('p')
+        infoHeading.id = 'infoHeading'
+        infoHeading.innerText = "Fun Fact"
+        animalFunFact.appendChild(infoHeading)
+        var animalFunFactText = document.createElement('p')
+        animalFunFactText.id = "animal-fun-fact-text"
+        animalFunFactText.innerText = animal.funFact
+        animalFunFact.appendChild(animalFunFactText)
+}
+    var backDiv = document.createElement('div')
+    backDiv.className = "back"
+    pageSection.appendChild(backDiv)
+      var h1 = document.createElement('h1')
+      backDiv.appendChild(h1)
+      h1.innerText = "Hello back"
+      var p = document.createElement('p')
+      backDiv.appendChild(p)
+}
+
+
+
+
+// end of new code
 var createHtml = function(){
 
   var backgroundImageWrapper = document.getElementById('background-image-wrapper')
