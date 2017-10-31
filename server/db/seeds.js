cbf593d633c06b4809cf3930457190e046ab1697
Simon Anger <simonanger89@gmail.com>
2017-10-31 13:48:21 +0000

    Fixed seeds:

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,7 +7,7 @@ db.facts.insert([
     area: "Jungle",
     name: "Macaw",
     type: "Bird",
-    img: "http://www.worldatlas.com/r/w728-h425-c728x425/upload/5e/d7/7c/shutterstock-153175595.jpg",
+    img: '/public/images/Macaw.png',
     funFact: "They can live up to 80 years old",
     group: "Flock"
   },
@@ -15,7 +15,7 @@ db.facts.insert([
     area: "Jungle",
     name: "Jaguar",
     type: "Mammal",
-    img: "http://www.worldatlas.com/r/w728-h425-c728x425/upload/c4/60/01/shutterstock-78913234.jpg",
+    img: '/public/images/tree-jaguar.png',
     funFact: "Like the tiger, jaguars enjoy water and are good at swimming",
     group: "Jamboree"
   },
@@ -23,7 +23,7 @@ db.facts.insert([
     area: "Jungle",
     name: "Poison Dart Frog",
     type: "Amphibian",
-    img: "http://www.worldatlas.com/r/w728-h425-c728x425/upload/cb/3b/40/shutterstock-424020241.jpg",
+    img: '/public/images/Dart-frog.png',
     funFact: "After the tadpoles have hatched, one of the parents will carry them on their back",
     group: "Army"
   }
