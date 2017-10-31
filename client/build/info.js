ef8a9149c44a7abe4f26703152a6aa4ac86be093
Iain Paterson <iain.paterson.cc@gmail.com>
2017-10-31 09:31:43 +0000

    Pushing new branch

--- client/build/info.js
+++ client/build/info.js
@@ -20,103 +20,108 @@ var renderJungleInfo = function(data){
   background.style.background = "url(./public/images/jungle.jpg)"
 
 
-
-  createHtml()
-
-  counter = 0
-  renderAnimalsInfo(data, counter)
-  counter ++
-
-//add if statement here
-  var nextButton = document.getElementById('button')
-  nextButton.addEventListener('click', function(){
-    renderAnimalsInfo(data, counter)
-    counter ++
-  })
-}
-
-var createHtml = function(){
-
   var backgroundImageWrapper = document.getElementById('background-image-wrapper')
   var journalImage = document.createElement('div')
   journalImage.id = 'journal-image'
   backgroundImageWrapper.appendChild(journalImage)
 
-    var buttonDiv = document.createElement('div')
-    buttonDiv.id = 'button'
-    journalImage.appendChild(buttonDiv)
-
-    var button = document.createElement('button')
-    button.innerText = 'Next'
-    buttonDiv.appendChild(button)
+  var buttonDiv = document.createElement('div')
+  buttonDiv.id = 'button'
+  journalImage.appendChild(buttonDiv)
 
 
-    var animalPictureWrapper = document.createElement('div')
-    animalPictureWrapper.id = 'animal-picture-wrapper'
-      var animalImg = document.createElement('img')
-      animalImg.id = 'animalImg'
-      var animalImgText = document.createElement('p')
-      animalImgText.id = 'name'
-      animalPictureWrapper.appendChild(animalImg)
-      animalPictureWrapper.appendChild(animalImgText)
-      journalImage.appendChild(animalPictureWrapper)
+  var button = document.createElement('button')
+  button.innerText = 'Next'
+  buttonDiv.appendChild(button)
 
 
-    var animalInfo = document.createElement('div')
-    animalInfo.id = 'animal-info'
-    journalImage.appendChild(animalInfo)
+  counter = 0
+  renderAnimalsInfo(data, counter)
+  counter ++
 
-      var animalType = document.createElement('div')
-      animalType.id = 'animalType'
-      animalInfo.appendChild(animalType)
-        var infoHeading = document.createElement('p')
-        infoHeading.id = 'infoHeading'
-        infoHeading.innerText = "Type of Animal"
-        animalType.appendChild(infoHeading)
+  var nextButton = document.getElementById('button')
+  nextButton.addEventListener('click', function(){
+    renderAnimalsInfo(data, counter)
+    counter ++
+  })
 
-      var animalGroup = document.createElement('div')
-      animalGroup.id = 'animalGroup'
-      animalInfo.appendChild(animalGroup)
-        var infoHeading = document.createElement('p')
-        infoHeading.id = 'infoHeading'
-        infoHeading.innerText = "Group Name"
-        animalGroup.appendChild(infoHeading)
-
-      var animalFunfact = document.createElement('div')
-      animalFunfact.id = 'animalFunfact'
-      animalInfo.appendChild(animalFunfact)
-        var infoHeading = document.createElement('p')
-        infoHeading.id = 'infoHeading'
-        infoHeading.innerText = "Fun Fact"
-        animalFunfact.appendChild(infoHeading)
 
 }
 
 var renderAnimalsInfo = function(animals, counter) {
 
   var journalImage = document.getElementById('journal-image')
+  // journalImage.innerHTML = "";
 
-var animalImg = document.getElementById('animalImg')
-  var animalImgText = document.getElementById('name')
+
+  var animalPictureWrapper = document.createElement('div')
+  animalPictureWrapper.id = 'animal-picture-wrapper'
+  var animalImg = document.createElement('img')
+  animalImg.id = 'animalImg'
+  var animalImgText = document.createElement('p')
+  animalImgText.id = 'name'
+  animalPictureWrapper.appendChild(animalImg)
+  animalPictureWrapper.appendChild(animalImgText)
+  journalImage.appendChild(animalPictureWrapper)
+
+
+
+  // var animalPic = document.getElementById('animalImg')
+
+
+
   var pictures = ['/public/images/Macaw.png','/public/images/tree-jaguar.png','/public/images/Dart-frog.png']
+
   animalImg.src = pictures[counter]
   animalImgText.innerText = animals[counter].name
 
-  var animalType = document.getElementById('animalType');
+
+  var animalInfo = document.createElement('div')
+  animalInfo.id = 'animal-info'
+  journalImage.appendChild(animalInfo)
+
+  var animalType = document.createElement('div')
+  animalType.id = 'animalType'
+  animalInfo.appendChild(animalType)
+  var infoHeading = document.createElement('p')
+  infoHeading.id = 'infoHeading'
+  infoHeading.innerText = "Type of Animal"
+  animalType.appendChild(infoHeading)
+
+  var animalGroup = document.createElement('div')
+  animalGroup.id = 'animalGroup'
+  animalInfo.appendChild(animalGroup)
+  var infoHeading = document.createElement('p')
+  infoHeading.id = 'infoHeading'
+  infoHeading.innerText = "Group Name"
+  animalGroup.appendChild(infoHeading)
+
+  var animalFunfact = document.createElement('div')
+  animalFunfact.id = 'animalFunfact'
+  animalInfo.appendChild(animalFunfact)
+  var infoHeading = document.createElement('p')
+  infoHeading.id = 'infoHeading'
+  infoHeading.innerText = "Fun Fact"
+  animalFunfact.appendChild(infoHeading)
+
+
+
+
+  // var animalType = document.getElementById('animalType');
   var animalTypeText = document.getElementById('animal-type-text')
   if (!animalTypeText) {
     animalTypeText = document.createElement('p')
     animalTypeText.id = 'animal-type-text'
   }
 
-  var animalGroup = document.getElementById('animalGroup');
+  // var animalGroup = document.getElementById('animalGroup');
   var animalGroupText = document.getElementById('animal-group-text')
   if (!animalGroupText) {
     animalGroupText = document.createElement('p')
     animalGroupText.id = 'animal-group-text'
   }
 
-  var animalFunfact = document.getElementById('animalFunfact');
+  // var animalFunfact = document.getElementById('animalFunfact');
   var animalFunfactText = document.getElementById('animal-funfact-text')
   if (!animalFunfactText) {
     animalFunfactText = document.createElement('p')
